a3801bc55f398036fbe3557950c43f32613eb770
Eldar <elderblr2@gmail.com>
2024-08-13 11:57:50 +0200

    Add modern css reset

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import 'modern-css-reset'
 
 const app = createApp(App)
 const pinia = createPinia()
